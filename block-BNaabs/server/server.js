8c00320b9b69fd839d9d0772fcd395c86d207fc6
abc <joel.rajesh13@gmail.com>
2021-06-14 22:17:07 +0530

    Routes in Express

--- block-BNaabs/server/server.js
+++ block-BNaabs/server/server.js
@@ -1,22 +1,25 @@
 var express = require('express')
-
+var logger = require('morgan')
 var app = express()
-
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(express.static(__dirname+"/public"))
+app.use(logger('dev'))
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + "/index.html")
 })
 app.get('/new', (req,res)=>{
     res.sendFile(__dirname + "/new.html")
 })
-app.get('/users:username',(req,res)=>{
+app.get('/users/:username',(req,res)=>{
     var username = req.params.username
     console.log(username)
-    res.json(username)
-    res.end()
+    res.send(username)
 })
-// app.post('/new', (req,res)=>{
-
-// })
+app.post('/new', (req,res)=>{
+    console.log(req.body)
+    res.json(req.body)
+})
 
 app.listen(3000, ()=>{
     console.log('listening to port 3000')
