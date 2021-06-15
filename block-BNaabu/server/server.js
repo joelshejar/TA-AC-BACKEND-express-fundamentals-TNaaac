aa0355874ec16ecc7954d3129fcb210e5898b136
abc <joel.rajesh13@gmail.com>
2021-06-15 10:45:32 +0530

    Error handler middlewares

--- block-BNaabu/server/server.js
+++ block-BNaabu/server/server.js
@@ -10,16 +10,17 @@ app.use((req, res, next) => {
     // other let it pass to next middleware by simply calling next()
     next();
   });
-app.use((req,res,next)=>{
-    res.send('Page not found')
-    next()
-})  
+ 
 app.get('/',(req,res)=>{
     res.send('Hey Bruh')
 })
 app.get('/about',(req,res)=>{
     res.send('Hey Bruh, wassup')
 })
+app.use((req,res,next)=>{
+    res.status(500).send('Page not found')
+    next()
+}) 
 app.use((err,req,res,next)=>{
     res.send(err)
 })
