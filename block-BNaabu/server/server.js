b351377874a8058f37fc61504c9fcaac24837223
abc <joel.rajesh13@gmail.com>
2021-06-15 10:35:35 +0530

    Error handler middlewares

---
+++ block-BNaabu/server/server.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use((req, res, next) => {
+    // if requested url is /admin throw error
+    if (req.url === "admin") {
+      return next("Unauthorized");
+    }
+    // other let it pass to next middleware by simply calling next()
+    next();
+  });
+app.use((req,res,next)=>{
+    res.send('Page not found')
+    next()
+})  
+app.get('/',(req,res)=>{
+    res.send('Hey Bruh')
+})
+app.get('/about',(req,res)=>{
+    res.send('Hey Bruh, wassup')
+})
+app.use((err,req,res,next)=>{
+    res.send(err)
+})
+app.listen(3000, ()=>{
+    console.log('listening to port 3000')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
